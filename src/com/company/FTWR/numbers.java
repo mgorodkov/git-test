033a9bcff121a2236be0f0d97b72b22f1097a882
mgorodkov <mgorodkov@gmail.com>
2021-02-22 15:07:34 +0300

    ferst test commit

---
+++ src/com/company/FTWR/numbers.java
@@ -0,0 +1,29 @@
+package com.company.FTWR;
+
+public class numbers {
+
+    static byte a = 5;
+    static short b = 400;
+    static int c = 35_000_000;
+    static long d = 1_000_000_000;
+
+    static char i = 63;
+
+    static float f = 7.5f;
+    static double r = 57.76;
+
+    static boolean g = true;
+
+
+    public static void main(String[] args) {
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(r);
+        System.out.println(g);
+
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        for (int i = 0; i < 5; i++) {
+
+            System.out.println("Hello worldd" + " раз " + random.nextInt( 10));
+            System.out.println("  ");
+        }
+    }
+}
